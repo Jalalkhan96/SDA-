0adbc1a3055a20eaae89ce696e6e1b7e67176236
Jalalkhan96 <Jalalkhan96@users.noreply.github.com>
2025-01-06 11:03:02 +0500

    LAB FINAL
    LAB FINAL
    BY JALAL KHAN

---
+++ LAB FINAL/CODE/Connection.java
@@ -0,0 +1,13 @@
+class Connection {
+    private String sensorName;
+    private String sensorID;
+
+    public Connection(String sensorName, String sensorID) {
+        this.sensorName = sensorName;
+        this.sensorID = sensorID;
+    }
+
+    public void connectToSensor() {
+        System.out.println("Connected to sensor: " + sensorName + " (ID: " + sensorID + ")");
+    }
+}
